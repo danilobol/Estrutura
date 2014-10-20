5e829356bf2f2dfd7791805f71f75b7b6e5c8b1c
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-20 03:48:46 -0200

    Questão 3 - Ordena 3 valores em ordem crescente.

---
+++ Lista 1/ex3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main() {
+	int a, b, c;
+	
+	int ordenar(int *a, int *b, int *c);
+	
+<<<<<<< HEAD
+=======
+	int ordenar(int *a, int *b, int *c);
+	
+>>>>>>> origin/master
+	scanf("%d %d %d", &a, &b, &c);
+	
+	ordenar(&a, &b, &c);
+	
+	printf ("%d %d %d",a,b,c);
+	
+	return 0;
+}
+
+int ordenar(int *a, int *b, int *c) {
+	if((*a == *b) & (*a == *c))
+		return 1;
+
+	if(*a > *b) {
+		*a += *b;
+		*b = *a - *b;
+		*a -= *b;
+	}
+	if(*b > *c) {
+		*b += *c;
+		*c = *b - *c;
+		*b -= *c;
+		
+		if(*a > *b) {
+			*a += *b;
+			*b = *a - *b;
+			*a -= *b;
+		}
+	}
+	
+	return 0;
+}
