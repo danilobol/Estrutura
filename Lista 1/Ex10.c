5b5111622a98afa1b68130290df945d71a400be2
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-20 03:52:01 -0200

    Questão 10 - Recebe dois numeros em A e B, e retorna a soma do dobro desses dois numeros.

---
+++ Lista 1/Ex10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+<<<<<<< HEAD
+#include <stdlib.h>
+=======
+>>>>>>> origin/master
+
+int main(){
+    
+    int SomaDobro (int *a, int *b, int *x);
+    int a, b, x;
+    scanf ("%d %d",&a,&b);
+    SomaDobro (&a, &b, &x);
+    
+    printf ("A soma do dobro de A e B eh %d \n", x);
+
+    return 0;
+}
+int SomaDobro (int *a, int *b, int *x){
+    *a = *a * 2;
+    *b = *b * 2;
+    *x = *a + *b;
+    
+    return *x;
+}
