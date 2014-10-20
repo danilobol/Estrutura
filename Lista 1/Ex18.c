c50d22f850ac3827c101ff57f210d83f50087590
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-20 03:54:17 -0200

    Questão 18 - Imprime os endereços de uma matriz 3x3

---
+++ Lista 1/Ex18.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	float a[3][3];
+	int i, j;
+	
+	for (i=0;i<3;i++){
+		for (j=0;j<3;j++){
+			printf ("%d ",&a[i][j]);
+		}
+		printf ("\n");
+	}
+	
+	return 0;
+}
