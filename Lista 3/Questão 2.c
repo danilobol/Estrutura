2c9cbcd70f5dcc8a90c449f50ed0e2e757dd418f
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-21 14:35:08 -0200

    Questão 2

---
+++ Lista 3/Questão 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    int n, i, *vetor[30], i=0, j, cont=0;
+	
+	
+    while (scanf ("%d",n) > 0){
+    	
+        vetor[i] = numero;
+        i++;
+        cont ++;
+
+    }
+
+    printf("\nO vetor digitado foi:\n");
+    for (j=0; j<cont ; j++)
+    {
+        printf("%d ", vetor[j]);
+    }
+
+    return 0;
+
+}
