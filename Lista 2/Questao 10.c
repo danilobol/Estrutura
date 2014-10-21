14a7cc3b56e904924a9050519b71675bb3b84320
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-21 14:17:26 -0200

    Questão 10

---
+++ Lista 2/Questao 10.c
@@ -0,0 +1,16 @@
+/*
+10) Escreva uma função que recebe uma string de caracteres e uma letra e devolve um vetor de inteiros contendo as posições 
+(índices no vetor da string) onde a letra foi encontrada e um inteiro contendo o tamanho do vetor criado 
+(total de letras iguais encontradas). 
+Utilize o retorno de um vetor para retornar os índices e um ponteiro para guardar o tamanho do vetor.
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	  char letra, str;
+	  int tam, i;
+	
+	return 0;
+}
