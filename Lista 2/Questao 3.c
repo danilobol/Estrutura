a768c315e6731d9c04c06f9bf8b5c933bd445285
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-21 14:14:47 -0200

    Questão 3

---
+++ Lista 2/Questao 3.c
@@ -0,0 +1,5 @@
+/* 3) Suponha que os elementos do vetor v são do tipo int e cada int ocupa 8 bytes
+no seu computador. Se o endereço de v[0] é 55000, qual o valor da
+expressão v + 3?
+
+RESPOSTA: O valor da expressão v + 3 seria 55024, pois o endereço dos elementos é sequencial, se o endereço de v[0] é 55000 e cada int ocupa 8 bytes, em cada posição soma mais 8. */
