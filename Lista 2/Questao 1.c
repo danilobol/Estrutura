f88ffa893cf4080c0cbd6e75995adb9d4bd1ba6a
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-21 14:14:30 -0200

    Questão 1

---
+++ Lista 2/Questao 1.c
@@ -0,0 +1,5 @@
+//Atividade 2
+//Questão 1:
+
+
+/*A diferença é que v[3] é um vetor com 3 posições ou um vetor na posição 3 e o v + 3 é uma operação aritimética onde vai somar o valor de uma variável mais o numero 3.*/
